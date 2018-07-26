2fe955e674c2cfff60df5c2a7381accc58880b59
imomarkssa <41760359+imomarkssa@users.noreply.github.com>
2018-07-26 23:42:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,406 @@ client.on('message', message => {
       }
 });
 
+client.on('message', message => {
+if(message.content.startsWith(prefix + 'move all')) {
+ if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send('__**ليس لديك صلاحيه**__');
+   if(!message.guild.member(client.user).hasPermission("MOVE_MEMBERS")) return message.reply("**لايوجد لدي صلاحية السحب**");
+if (message.member.voiceChannel == null) return message.channel.send(`**الرجاء الدخول لروم صوتي**`)
+ var author = message.member.voiceChannelID;
+ var m = message.guild.members.filter(m=>m.voiceChannel)
+ message.guild.members.filter(m=>m.voiceChannel).forEach(m => {
+ m.setVoiceChannel(author)
+ })
+ message.channel.send(`**تم سحب جميع الأعضاء إليك**`)
 
+
+ }
+   });
+
+
+
+const cuttweet = [
+    'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+    'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
+    'كت تويت | الحرية لـ ... ؟',
+    'كت تويت | قناة الكرتون المفضلة في طفولتك؟',
+    'كت تويت ‏| كلمة للصُداع؟',
+    'كت تويت ‏| ما الشيء الذي يُفارقك؟',
+    'كت تويت | موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+    'كت تويت ‏| أيهما ينتصر، الكبرياء أم الحب؟',
+    'كت تويت | بعد ١٠ سنين ايش بتكون ؟',
+    'كت تويت ‏| مِن أغرب وأجمل الأسماء التي مرت عل��ك؟',
+    '‏كت تويت | عمرك شلت مصيبة عن شخص برغبتك ؟',
+    'كت تويت | أكثر سؤال وجِّه إليك مؤخرًا؟',
+    '‏كت تويت | ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+    '‏كت تويت | وش يفسد الصداقة؟',
+    '‏كت تويت | شخص لاترفض له طلبا ؟',
+    '‏كت تويت | كم مره خسرت شخص تحبه؟.',
+    '‏كت تويت | كيف تتعامل مع الاشخاص السلبيين ؟',
+    '‏كت تويت | كلمة تشعر بالخجل اذا قيلت لك؟',
+    '‏كت تويت | جسمك اكبر من عٌمرك او العكسّ ؟!',
+    '‏كت تويت |أقوى كذبة مشت عليك ؟',
+    '‏كت تو��ت | تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+    'كت تويت | هل حدث وضحيت من أجل شخصٍ أحببت؟',
+    '‏كت تويت | أكثر تطبيق تستخدمه مؤخرًا؟',
+    '���كت ��ويت | ‏اكثر شي يرضيك ��ذا زعلت بدون تفكير ؟',
+    '‏كت تويت | وش محتاج عشان تكون مبسو�� ؟',
+    '‏كت ت��يت | مطلبك الوحيد الحين ؟',
+    '‏كت تويت | هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
+]
+
+client.on('message', message => {
+  if (message.content.startsWith("*كت تويت")) {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+ var embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+  .setThumbnail(message.author.avatarURL)
+.addField('لعبه كت تويت',
+ `${cuttweet[Math.floor(Math.random() * cuttweet.length)]}`)
+ message.channel.sendEmbed(embed);
+ console.log('[id] Send By: ' + message.author.username)
+   }
+});
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content.includes('discord.gg')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** :angry: !ممنوع النشر هنا :angry: ! **`)
+    }
+}
+});
+client.on('message', message => {
+    if(message.content.includes('https://')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** :angry: !ممنوع النشر هنا :angry: ! **`)
+    }
+}
+});
+
+client.on('message', message => {
+    if(message.content.includes('https://www.youtube.com/')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** :angry: !ممنوع النشر هنا :angry: ! **`)
+    }
+}
+});
+
+client.on('message', message => {
+    if(message.content.includes('!p')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`**!هذا ليس مكان للموسيقى!**`)
+    }
+}
+});
+
+client.on('message', message => {
+    if(message.content.includes('!play')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`**!هذا ليس مكان للموسيقى!**`)
+    }
+}
+});
+
+client.on('message', message => {
+    if(message.content.includes('https://www.youtube.com/user/')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** :angry: !ممنوع النشر هنا :angry: ! **`)
+    }
+}
+});
+
+client.on('message', message => {
+    if(message.content.includes('https://youtu.be/')){
+                                            if(!message.channel.guild) return message.reply('** advertising me on DM ? :thinking:   **');
+        if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+        message.delete()
+    return message.reply(`** :angry: !ممنوع النشر هنا :angry: ! **`)
+    }
+}
+});
+
+client.on('ready', () => {
+  client.user.setGame(`Bot By Alasiri |*help`,'https://www.twitch.tv/v5bz');
+});
+
+
+
+             client.on('message', message => {
+                if(message.content === prefix + "invite") {
+                    let embed = new Discord.RichEmbed ()
+                    embed.setTitle("للدخول لسيرفر الدعم الفني")
+                    .setURL("https://discord.gg/sksRqkv");
+
+                   message.channel.sendEmbed(embed);
+                  }
+});
+
+
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', '✕ | prison'));
+     message.member.removeRole(message.guild.roles.find('name', '㋡ | GHOST MEMBERS'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`الاداره`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500)
+
+
+    }
+    }
+})
+
+
+
+
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('https://youtu.be/')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', '✕ | prison'));
+       message.member.removeRole(message.guild.roles.find('name', '㋡ | GHOST MEMBERS'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`الاداره`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500)
+
+
+    }
+    }
+})
+
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('https://www.youtube.com/user/')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', '✕ | prison'));
+       message.member.removeRole(message.guild.roles.find('name', '㋡ | GHOST MEMBERS'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`الاداره`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500)
+
+
+    }
+    }
+})
+
+
+
+
+
+
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('https://www.youtube.com/')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', '✕ | prison'));
+     message.member.removeRole(message.guild.roles.find('name', '㋡ | GHOST MEMBERS'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(":x: | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`الاداره`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500)
+
+
+    }
+    }
+})
+
+client.on('message', message => {
+    if(message.content === "*bot") {
+        const embed = new Discord.RichEmbed()
+        .setColor("#00FFFF")
+  .addField('**الذاكرة المستخدمة 💾**', `${(process.memoryUsage().rss / 1000000).toFixed()}MB`, true)
+         .addField('**سرعة الاتصال📡**' , `${Date.now() - message.createdTimestamp}` + ' ms')
+        .addField('**استخدام المعالج💿**', `${(process.cpuUsage().rss / 10000).toFixed()}%`, true)
+        .addField('**:globe_with_meridians: عدد السيرفرات**' , `${client.guilds.size}`, true)
+        .addField('**عدد المستخدمين 👥 **' , `${client.users.size}`, true)
+               message.channel.sendEmbed(embed);
+           }
+});
+
+client.on('ready', () => {
+  console.log('By LeHunter Enjoy');
+});
+
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`عضو جديد`)
+    .setDescription(`اهلا بك في السيرفر منور :heart:`)
+    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter('GhostGamers', 'https://cdn.discordapp.com/attachments/437020575463112705/437944895316164610/image.png')
+
+var channel =member.guild.channels.find('name', 'public_chat')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+client.on('message' , function (message){
+      var token = 'NDU4NTY1MzIyMDE0NzIwMDAw.DjrRXQ.hOSwV07h6vunPRjs5H1-uvdo2cA'
+      if(message.content === 'res') {
+if(message.author.id !== '321476121956974595') return message.reply('**الامر خاص بـ صاحب البوت وشكرا**');
+          client.destroy();
+          client.login(token) // لا تغيرها
+var time = 7200000;
+client.setInterval(function() {
+    client.destroy();
+    client.login(token) // لا تغيرها
+  }, time);
+}
+})
+
+ client.on("ready", () => {
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("359674601392832512");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            dat[Inv] = Invite.uses;
+
+        });
+
+    });
+
+});
+
+
+
+
+
+
+
+client.on("guildMemberAdd", (member) => {
+
+    let channel = member.guild.channels.get("471393414395461634");
+
+    if (!channel) {
+
+        console.log("!the channel id it's not correct");
+
+        return;
+
+    }
+
+    if (member.id == client.user.id) {
+
+        return;
+
+    }
+
+    console.log('-');
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("359674601392832512");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            if (dat[Inv])
+
+                if (dat[Inv] < Invite.uses) {
+
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;
+
+ }
+
+            dat[Inv] = Invite.uses;
+
+
+
+       });
+
+    });
+
+});
+
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='*Members')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTitle(':tulip:| Members info')
+      .addBlankField(true)
+      .addField('عدد اعضاء السيرفر',`${message.guild.memberCount}`)
+      message.channel.send(IzRo);
+    });
+
+client.on('message', message => {
+            if (message.content.startsWith(prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     ***Members** ' ,' **لرؤيه عدد الاعضاء** ')
+.addField('     ***bot**  ' ,' **احصائيات البوت**  ')
+.addField('     ***كت تويت**  ' ,' **لعبه كت تويت**  ')
+.addField('     **More** ' ,' **Soon** ')
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
